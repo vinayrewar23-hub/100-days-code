57d7b371f8a61602f428b72bb4899b6c5fb43d24
vinayrewar23-hub <vinayrewar23@gmail.com>
2025-09-25 13:34:47 +0530

    Create Q2.c

---
+++ day 6 to 10/day 6/Q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Input a number
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Nested if-else to check sign
+    if (num >= 0) {
+        if (num == 0) {
+            printf("The number is Zero.\n");
+        } else {
+            printf("The number is Positive.\n");
+        }
+    } else {
+        printf("The number is Negative.\n");
+    }
+
+    return 0;
+}
