6f7038c54d273f9921d2cdac6cd061a72665d53c
vinayrewar23-hub <vinayrewar23@gmail.com>
2025-09-25 14:18:23 +0530

    Create Q1.c

---
+++ day 6 to 10/day 8/Q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    // Input a character
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check the type of character
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("It is an uppercase alphabet.\n");
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("It is a lowercase alphabet.\n");
+    }
+    else if (ch >= '0' && ch <= '9') {
+        printf("It is a digit.\n");
+    }
+    else {
+        printf("It is a special character.\n");
+    }
+
+    return 0;
+}
