7983e42ed2a8bac4caf4d051f0b4d93398615419
vinayrewar23-hub <vinayrewar23@gmail.com>
2025-09-25 13:37:16 +0530

    Create Q1.c

---
+++ day 6 to 10/day 7/Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    // Input year
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Check leap year conditions
+    if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
+
+
+
+example =Enter a year: 2024
+2024 is a leap year.
+
+Enter a year: 2100
+2100 is not a leap year.
