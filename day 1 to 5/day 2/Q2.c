d6041d2ff882ff488bc1e5d7c724d2b529de12be
vinayrewar23-hub <vinayrewar23@gmail.com>
2025-09-25 13:20:20 +0530

    Create Q2.c

---
+++ day 1 to 5/day 2/Q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define PI 3.14159
+
+int main() {
+    float radius, area, circumference;
+
+    // Input radius
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    // Calculations
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    // Output results
+    printf("Area of the circle = %.2f\n", area);
+    printf("Circumference of the circle = %.2f\n", circumference);
+
+    return 0;
+}
